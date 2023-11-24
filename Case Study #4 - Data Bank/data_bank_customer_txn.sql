89ecdbf92b9fc434ec62e842db57c2c5203b5598
Duong Le Thanh Truc <145547282+thanhtruchhh@users.noreply.github.com>
2023-11-24 13:29:28 +0700

    Update query 4, 5

--- Case Study #4 - Data Bank/data_bank_customer_txn.sql
+++ Case Study #4 - Data Bank/data_bank_customer_txn.sql
@@ -1,5 +1,5 @@
 -- Solved on PostgreSQL v13 by Duong Le Thanh Truc
--- Date: 2023/11/22
+-- Date: 2023/11/24
 
 /* --------------------
    B. Customer Transactions
@@ -62,48 +62,98 @@ WHERE
 GROUP BY 1
 ORDER BY 1;
 
+-- Solved on PostgreSQL v13 by Duong Le Thanh Truc
+-- Date: 2023/11/22
+
+SET search_path = data_bank;
+
 -- 4. What is the closing balance for each customer at the end of the month?
 
--- Get the end of the month and calculate balance basing on income/ outcome of each transaction
-WITH txn_balances AS (
-  SELECT
-      customer_id,
-      DATE_TRUNC('month', txn_date) + INTERVAL '1 month - 1 day' AS end_of_mth,
-      CASE
-          WHEN txn_type = 'deposit' THEN txn_amount
-          ELSE -txn_amount
-      END AS txn_balance
-  FROM customer_transactions
-),
+-- Create temp table to calc the closing balance for each customer at the end of the month --> Use this table for query 4, 5
+CREATE TEMP TABLE customer_monthly_balance AS (
+  
+  -- Get the end of the month and calculate balance basing on income/ outcome of each transaction
+  WITH txn_balances AS (
+    SELECT
+        customer_id,
+        DATE_TRUNC('month', txn_date) + INTERVAL '1 month - 1 day' AS end_of_mth,
+        CASE
+            WHEN txn_type = 'deposit' THEN txn_amount
+            ELSE -txn_amount
+        END AS txn_balance
+    FROM customer_transactions
+  ),
 
--- Calc transaction balance changes in seperate months
-monthly_changes AS (
-  SELECT
+  -- Calc transaction balance changes in seperate months
+  monthly_changes AS (
+    SELECT
+        customer_id,
+        end_of_mth,
+        SUM(txn_balance) AS monthly_change
+    FROM txn_balances
+    GROUP BY 1, 2
+  ),
+
+  -- Create a table with customer_id and the ending date of each month from the minimum txn_date to the maximum txn_date
+  month_end_series AS (
+  SELECT 
+      customer_id,
+      DATE_TRUNC('month', GENERATE_SERIES(MIN(txn_date), MAX(txn_date), INTERVAL '1 month')) + INTERVAL '1 month - 1 day' AS end_of_mth
+  FROM customer_transactions
+  GROUP BY 1
+  )
+
+  SELECT 
       customer_id,
       end_of_mth,
-      SUM(txn_balance) AS monthly_change
-  FROM txn_balances
-  GROUP BY 1, 2
-),
+      SUM(monthly_change) OVER(
+          PARTITION BY customer_id
+          ORDER BY end_of_mth
+          ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+      ) AS closing_balance
+  FROM monthly_changes
+  FULL JOIN month_end_series USING (customer_id, end_of_mth)
+);
 
--- Create a table with customer_id and the ending date of each month from the minimum txn_date to the maximum txn_date
-month_end_series AS (
-SELECT 
-	customer_id,
-	DATE_TRUNC('month', GENERATE_SERIES(MIN(txn_date), MAX(txn_date), INTERVAL '1 month')) + INTERVAL '1 month - 1 day' AS end_of_mth
-FROM customer_transactions
-GROUP BY 1
-)
-
-SELECT 
-	customer_id,
-	end_of_mth,
-    SUM(monthly_change) OVER(
-		PARTITION BY customer_id
-		ORDER BY end_of_mth
-		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-	) AS monthly_balance
-FROM monthly_changes
-FULL JOIN month_end_series USING (customer_id, end_of_mth)
+-- Only show the result for customer 1, 2, 3
+SELECT *
+FROM customer_monthly_balance
 WHERE customer_id IN ('1', '2', '3')
 ORDER BY 1, 2;
+
+
+-- 5. What is the percentage of customers who increase their closing balance by more than 5%?
+
+-- Growth rate = 100 * (Balance in cur period - Balance in prev period) / abs(Balance in prev period)
+
+-- Get the closing balance in the previous month
+WITH prev_month_balance AS (
+  SELECT 
+      *,
+      LAG(closing_balance) OVER(
+          PARTITION BY customer_id
+          ORDER BY end_of_mth
+      ) AS prev_closing_balance
+  FROM customer_monthly_balance
+),
+
+-- 2 customers have a closing balance = 0
+-- SELECT DIV(9, 0)* FROM customer_monthly_balance WHERE customer_id IN ('250', '306') ORDER BY 1, 2;
+
+-- Calc % change of closing balance month over month
+monthly_change AS (
+  SELECT 
+      customer_id,
+      end_of_mth,
+      -- Add a very small number to both the value of the current period and the value of the previous period to avoid division by 0.
+      (closing_balance - prev_closing_balance) * 100.0 / ABS(prev_closing_balance + 0.0000000000000001) AS change_pct
+  FROM prev_month_balance
+)
+
+SELECT
+	ROUND(
+		100.0 * COUNT(DISTINCT customer_id) / (SELECT COUNT (DISTINCT customer_id) FROM customer_monthly_balance),
+	2)
+FROM monthly_change
+WHERE change_pct > 5;
+
